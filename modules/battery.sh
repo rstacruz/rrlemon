842ea45cb00433a05e2cb4a6540866bc7a41b111
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-07-06 01:24:50 +1000

    Reduce appearance of battery meter

--- modules/battery.sh
+++ modules/battery.sh
@@ -1,6 +1,9 @@
 battery() {
-  # 85%
+  # battery percent ('85%')
   local percent="$(cat /sys/class/power_supply/BAT0/capacity)%"
+
+  # If connected ('1' or '0')
+  local adapter="$(cat /sys/class/power_supply/ADP1/online)"
 
   # Discharging
   local status="$(cat /sys/class/power_supply/BAT0/status)"
@@ -17,7 +20,11 @@ battery() {
 
   local suffix="$(if [[ "$status" == "Discharging" ]]; then echo "left"; else echo "to full"; fi)"
 
-  echo -ne "${MUTE}${time} ${suffix}${SPACE}${percent}"
+  if [[ "$percent" == "100%" ]]; then
+    echo -ne "${MUTE}${percent}"
+  else
+    echo -ne "${MUTE}${time} ${suffix}${SPACE}${percent}"
+  fi
 }
 
 while true; do
